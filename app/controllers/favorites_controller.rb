98aa1eba5b6e8dde5db7788b28c50170960a6791
Alex Persaud <alex_persaud01@yahoo.com>
2017-05-26 12:32:57 -0400

    Star and unstar

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,3 +2,9 @@ post '/favorites' do
   Favorite.create(user: current_user, cocktail_id: params[:cocktail_id])
   redirect "/users/#{current_user.username}"
 end
+
+post '/favorites/remove' do
+  favorite = Favorite.find_by(user: current_user, cocktail_id: params[:cocktail_id])
+  favorite.destroy
+  redirect "/users/#{current_user.username}"
+end
