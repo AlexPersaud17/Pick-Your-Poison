b9fac675a1074c9e45c6e497109b02af40bfc18b
Alex Persaud <alex_persaud01@yahoo.com>
2017-05-26 09:39:57 -0400

    Edge cases

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -3,14 +3,14 @@ get '/' do
 end
 
 post '/search' do
-  search_query = params[:search_query].gsub(' ', '%20')
-  uri = URI.parse("http://addb.absolutdrinks.com/quickSearch/drinks/#{search_query}/?apiKey=#{ENV['API_KEY']}")
+  @search_query = params[:search_query].gsub(' ', '%20')
+  uri = URI.parse("http://addb.absolutdrinks.com/quickSearch/drinks/#{@search_query}/?apiKey=#{ENV['API_KEY']}")
   response = Net::HTTP.get_response(uri)
   @body = JSON.parse(response.body)
   search_results = []
   @body["result"].each do |drink|
     cocktail =  Cocktail.find_or_create_by(name: drink["name"], isAlcoholic: drink["isAlcoholic"], description: drink["descriptionPlain"])
-    drink["ingredients"].each do |ingredient| 
+    drink["ingredients"].each do |ingredient|
       cocktail.ingredients << ingredient["textPlain"] unless cocktail.ingredients.include?(ingredient["textPlain"])
     end
     cocktail.save
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,3 @@
-addb.init({
-    appId: 5
-});
-
 $(document).ready(function() {
 
 });
